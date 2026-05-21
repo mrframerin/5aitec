28c296cf279a6aedbe06d068a392bc4489a8a054
Anton-dev12 <titus@ztrategize.com>
2026-05-21 15:30:41 +0530

    Changed image

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
-const config: NextConfig = {
-  reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: "/_next/static/media/logo.0ctv.ko5~mr~7.svg",
-        destination: "/textures/logo.0ctv.ko5~mr~7.svg",
-      },
-      {
-        source: "/_next/static/media/:path*",
-        destination: "https://www.shader.se/_next/static/media/:path*",
-      },
-    ];
-  },
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
 };
 
-export default config;
+export default nextConfig;
